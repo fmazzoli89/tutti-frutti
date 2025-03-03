15c0c7e90a4fbfe2407cdeb58aab4627864d2fd6
Federico Mazzoli <fmazzoli89@gmail.com>
2025-03-03 10:43:20 -0300

    Simplify MainScreen component by removing unused imports and state

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,17 +1,7 @@
-import React, { useState } from 'react';
-import { useGame } from '../context/GameContext';
-import EngineToggle from './EngineToggle';
-import Game from './Game';
 import '../styles/MainScreen.css';
+import Game from './Game';
 
 const MainScreen = () => {
-  const { startGame } = useGame();
-  const [showApiSettings, setShowApiSettings] = useState(false);
-
-  const toggleApiSettings = () => {
-    setShowApiSettings(!showApiSettings);
-  };
-
   return (
     <div className="main-screen">
       <div className="game-container">
